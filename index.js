faf32817f1a1990ac017e9f85586e439efce2a15
umfowenu <marius@shaw.ca>
2025-07-14 19:54:25 -0700

    Add CORS headers to fix WordPress API connection

--- index.js
+++ index.js
@@ -2,6 +2,21 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
+// ADD CORS HEADERS - Insert this section right here
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://ebiz360.ca');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  
+  // Handle preflight requests
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
+
 // Stripe webhooks need raw, others can use json
 app.use((req, res, next) => {
   if (req.originalUrl === '/api/stripe/webhook') {
@@ -10,7 +25,6 @@ app.use((req, res, next) => {
     express.json()(req, res, next);
   }
 });
-
 
 // Import routes
 const routes = require('./routes');
